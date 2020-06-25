0462700d633bcfe687ceff1016acf5ec979d1bf5
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-06-25 11:51:59 +0100

    added header

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
-const PageOne = () => {
-    return (
-    <div>
-        PageOne</div>
-    );
-};
-
-const PageTwo = () => {
-    return <div>
-    PageTwo
-     <button> click me</button>
-        </div>
-};
 
 const App = () => {
     return (
-    <div>
+    <div className="ui container">
         <BrowserRouter>
         <div> 
-        <Route path="/" exact component={PageOne} />
-        <Route path="/pagetwo" exact component={PageTwo} />
+        <Header/>
+        <Route path="/" exact component={StreamList}/>
+        <Route path="/streams/delete" exact component={StreamDelete}/>
+        <Route path="/streams/edit" exact  component={StreamEdit}/>
+        <Route path="/streams/new" exact  component={StreamCreate}/>
+        <Route path="/streams/show" exact  component={StreamShow}/>
         </div>
         </BrowserRouter>
         </div>
